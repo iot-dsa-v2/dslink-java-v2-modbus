c04616b73ccd8bf061e2d91b65f89ee0f64351cc
Daniel Shapiro <d.shapiro@berkeley.edu>
2017-12-11 16:20:15 -0800

    initial commit

---
+++ src/main/java/org/iot/dsa/dslink/modbus/RootNode.java
@@ -0,0 +1,12 @@
+package org.iot.dsa.dslink.modbus;
+
+import org.iot.dsa.dslink.DSRootNode;
+
+public class RootNode extends DSRootNode {
+    
+    @Override
+    protected void declareDefaults() {
+        
+    }
+
+}
