78835d99a408213ef4d30794e06a0471f7d23558
JurisPuchin <juris.puchin@gmail.com>
2018-01-08 17:05:38 -0800

    Parameters added to SlaveDeviceNode.

--- src/main/java/org/iot/dsa/dslink/modbus/SlaveDeviceNode.java
+++ src/main/java/org/iot/dsa/dslink/modbus/SlaveDeviceNode.java
@@ -1,6 +1,9 @@
 package org.iot.dsa.dslink.modbus;
 
-import org.iot.dsa.node.*;
+import org.iot.dsa.node.DSIObject;
+import org.iot.dsa.node.DSLong;
+import org.iot.dsa.node.DSMap;
+import org.iot.dsa.node.DSNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +15,33 @@ import java.util.List;
 public class SlaveDeviceNode extends DSNode {
 
     public static List<ParameterDefinition> parameterDefinitions = new ArrayList<ParameterDefinition>();
+
     static {
         parameterDefinitions.add(ParameterDefinition.makeParamWithDefault(Constants.SLAVE_ID, DSLong.valueOf(Constants.DEFAULT_SLAVE_ID), null, null));
         parameterDefinitions.add(ParameterDefinition.makeParamWithDefault(Constants.IP_PORT, DSLong.valueOf(Constants.DEFAULT_SLAVE_IP_PORT), null, null));
     }
 
+    DSMap parameters;
+
+    public SlaveDeviceNode() {
+
+    }
+
     public SlaveDeviceNode(DSMap parameters) {
+        this.parameters = parameters;
+    }
+
+    @Override
+    protected void onStarted() {
+        if (this.parameters == null) {
+            DSIObject o = get(Constants.PARAMETERS);
+            if (o instanceof DSMap) {
+                this.parameters = (DSMap) o;
+            }
+            Util.verifyParameters(parameters, parameterDefinitions);
+        } else {
+            Util.verifyParameters(parameters, parameterDefinitions);
+            put(Constants.PARAMETERS, parameters.copy());
+        }
     }
 }
