2458607aa5ba77da736ca197e857e780ec6cecc5
Daniel Shapiro <d.shapiro@berkeley.edu>
2018-01-16 16:11:08 -0800

    make serial port rescan update serial slave add/edit actions

--- src/main/java/org/iot/dsa/dslink/modbus/RootNode.java
+++ src/main/java/org/iot/dsa/dslink/modbus/RootNode.java
@@ -35,12 +35,16 @@ public class RootNode extends DSRootNode {
 
     private void rescanSerialPorts() {
         put(Constants.ACTION_ADD_SERIAL, DFUtil.getAddAction(SerialConnectionNode.class));
+        put(Constants.ACTION_ADD_SERIAL_SLAVES, DFUtil.getAddAction(SerialSlaveConnectionNode.class));
         for (DSInfo info: this) {
             if (info.isNode()) {
                 DSNode n = info.getNode();
                 if (n instanceof SerialConnectionNode) {
                     SerialConnectionNode scn = (SerialConnectionNode) n;
                     scn.put(DFHelpers.ACTION_EDIT, scn.makeEditAction());
+                } else if (n instanceof SerialSlaveConnectionNode) {
+                    SerialSlaveConnectionNode sscn = (SerialSlaveConnectionNode) n;
+                    sscn.put(DFHelpers.ACTION_EDIT, sscn.makeEditAction());
                 }
             }
         }
