5a2d139640832df06e4fff608f9f7bac6a29bc8e
Daniel Shapiro <d.shapiro@berkeley.edu>
2018-01-18 16:54:53 -0800

    name() instead of toString()

--- src/test/java/org/iot/dsa/dslink/modbus/utils/DataTypeParameterTests.java
+++ src/test/java/org/iot/dsa/dslink/modbus/utils/DataTypeParameterTests.java
@@ -12,7 +12,7 @@ public class DataTypeParameterTests {
     @Test
     public void verifyDatatypeMissing() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.name());
         try {
             dtp.verify(parameters);
             fail("Expected verify to fail with an exception");
@@ -24,7 +24,7 @@ public class DataTypeParameterTests {
     @Test
     public void verifyDatatypeNotString() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.toString()).put(Constants.POINT_DATA_TYPE, 23);
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.name()).put(Constants.POINT_DATA_TYPE, 23);
         try {
             dtp.verify(parameters);
             fail("Expected verify to fail with an exception");
@@ -36,7 +36,7 @@ public class DataTypeParameterTests {
     @Test
     public void verifyDatatypeInvalidString() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.toString()).put(Constants.POINT_DATA_TYPE, "FIVE_BYTE_INT_SIGNED");
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.name()).put(Constants.POINT_DATA_TYPE, "FIVE_BYTE_INT_SIGNED");
         try {
             dtp.verify(parameters);
             fail("Expected verify to fail with an exception");
@@ -48,21 +48,21 @@ public class DataTypeParameterTests {
     @Test
     public void verifyCoilBinary() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyDiscreteBinary() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.DISCRETE.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.DISCRETE.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyCoilInt() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.FOUR_BYTE_INT_UNSIGNED.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.FOUR_BYTE_INT_UNSIGNED.name());
         try {
             dtp.verify(parameters);
             fail("Expected verify to fail with an exception");
@@ -74,7 +74,7 @@ public class DataTypeParameterTests {
     @Test
     public void verifyCoilString() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.CHAR.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.COIL.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.CHAR.name());
         try {
             dtp.verify(parameters);
             fail("Expected verify to fail with an exception");
@@ -86,7 +86,7 @@ public class DataTypeParameterTests {
     @Test
     public void verifyDiscreteInt() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.DISCRETE.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.TWO_BYTE_INT_UNSIGNED_SWAPPED.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.DISCRETE.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.TWO_BYTE_INT_UNSIGNED_SWAPPED.name());
         try {
             dtp.verify(parameters);
             fail("Expected verify to fail with an exception");
@@ -98,7 +98,7 @@ public class DataTypeParameterTests {
     @Test
     public void verifyDiscreteString() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.DISCRETE.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.VARCHAR.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.DISCRETE.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.VARCHAR.name());
         try {
             dtp.verify(parameters);
             fail("Expected verify to fail with an exception");
@@ -110,56 +110,56 @@ public class DataTypeParameterTests {
     @Test
     public void verifyHoldingBinary() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyHoldingInt() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_INT_SIGNED_SWAPPED.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_INT_SIGNED_SWAPPED.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyHoldingFloat() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_FLOAT.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_FLOAT.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyHoldingString() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.CHAR.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.HOLDING.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.CHAR.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyInputBinary() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.BINARY.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyInputInt() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_INT_SIGNED_SWAPPED.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_INT_SIGNED_SWAPPED.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyInputFloat() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_FLOAT.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.EIGHT_BYTE_FLOAT.name());
         dtp.verify(parameters);
     }
     
     @Test
     public void verifyInputString() {
         DataTypeParameter dtp = new DataTypeParameter(null, null);
-        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.toString()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.CHAR.toString());
+        DSMap parameters = new DSMap().put(Constants.POINT_OBJECT_TYPE, PointType.INPUT.name()).put(Constants.POINT_DATA_TYPE, DataTypeEnum.CHAR.name());
         dtp.verify(parameters);
     }
 
