f0afd9e37258425426c167c832ffdb19bcbaa0a3
JurisPuchin <juris.puchin@gmail.com>
2018-02-23 15:54:39 -0800

    Enabled adjustment of subscription delay.

--- src/test/java/org/iot/dsa/dslink/modbus/ModbusFuzzTest.java
+++ src/test/java/org/iot/dsa/dslink/modbus/ModbusFuzzTest.java
@@ -29,6 +29,9 @@ public class ModbusFuzzTest {
     @Before
     public void setUp() {
         if (REDO_FUZZ) {
+            FuzzTest.SUBSCRIBE_DELAY_RETRIES = 100;
+            FuzzTest.SUBSCRIBE_DELAY_WAIT_MILIS = 300;
+            FuzzTest.INTERSTEP_WAIT_TIME = FuzzTest.PING_POLL_RATE * 4;
             PrintWriter writer = FuzzTest.getNewPrintWriter(FuzzTest.TESTING_OUT_FILENAME);
             FuzzTest.builFuzzDoubleTree(TEST_LENGTH, writer, new MainNode(), new ModbusSlaveTestingIPConnection(), new ModbusFuzzNodeAction());
             writer.close();
